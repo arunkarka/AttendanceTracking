6901f3eb259d3bf290e422faeddbc3bbfdcc86bd
arunkarka <arunkarka@gmail.com>
2015-06-24 15:30:07 -0500

    Added List View for Student list in Teacher Activity

--- app/src/main/java/com/example/arunkarka/attendancetracking/TeacherActivity.java
+++ app/src/main/java/com/example/arunkarka/attendancetracking/TeacherActivity.java
@@ -1,9 +1,19 @@
 package com.example.arunkarka.attendancetracking;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 
 public class TeacherActivity extends ActionBarActivity {
@@ -12,7 +22,25 @@ public class TeacherActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+        String[] values = new String[] { "Monica", "Kavya", "Arjun",
+                "John", "Abbas", "Yuvraj", "Modi", "Jaya",
+                "Kavitha", "Tommy" };
+        ListView lv = (ListView) findViewById(R.id.student_list);
+        lv.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.student_name, values));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView< ?> parent, View view, int position, long id) {
+
+                String studentName = ((TextView) view.findViewById(R.id.student_name)).getText().toString();
+                Intent launchStudentAct = new Intent(getApplicationContext(), StudentActivity.class);
+                launchStudentAct.putExtra("STUDENT_NAME", studentName);
+                startActivity(launchStudentAct);
+            }
+        });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
